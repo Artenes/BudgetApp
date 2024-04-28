f24c57d5dc9036f1a9cbf6a568bd4f9e69575ff1
Artenes Nogueira <artenesama@gmail.com>
2024-04-28 12:56:08 -0400

    Fix layout for load screen

--- app/src/main/java/xyz/artenes/budget/app/load/InitialLoadScreen.kt
+++ app/src/main/java/xyz/artenes/budget/app/load/InitialLoadScreen.kt
@@ -1,5 +1,6 @@
 package xyz.artenes.budget.app.load
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -39,9 +41,15 @@ fun InitialLoadScreen(
             contentAlignment = Alignment.Center,
         ) {
 
-            Column {
+            Column(
+                verticalArrangement = Arrangement.Center
+            ) {
                 CircularProgressIndicator(
                     color = MaterialTheme.colorScheme.onBackground
+                )
+                Text(
+                    text = "Loading initial data",
+                    color = CustomColorScheme.textColor()
                 )
             }
 
