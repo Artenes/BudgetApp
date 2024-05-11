3b00864801fd4804fc1c67a29c894ff95c3e8da9
Artenes Nogueira <artenesama@gmail.com>
2024-05-11 14:36:37 -0400

    Replace save icon with FAB in category screen

--- app/src/main/java/xyz/artenes/budget/app/category/CategoryEditorScreen.kt
+++ app/src/main/java/xyz/artenes/budget/app/category/CategoryEditorScreen.kt
@@ -5,9 +5,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -137,16 +138,17 @@ fun CategoryEditorScreen(
                             )
                         }
                     }
-                    IconButton(onClick = viewModel::save) {
-                        Icon(
-                            imageVector = Icons.Filled.Save,
-                            contentDescription = ""
-                        )
-                    }
                 }
             )
 
         },
+        floatingActionButton = {
+
+            FloatingActionButton(onClick = { viewModel.save() }) {
+                Icon(imageVector = Icons.Filled.Check, contentDescription = "")
+            }
+
+        }
     ) { screen ->
 
         Column(modifier = Modifier.padding(screen)) {
